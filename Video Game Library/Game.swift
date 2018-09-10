d3363b4bc915c87b6dd1a0933157374054b05f6d
Zachary Harper <zacharyharper@macbook-pro-8.local>
2018-09-10 13:17:47 -0500

    ADDED GAME CLASS

---
+++ Video Game Library/Game.swift
@@ -0,0 +1,50 @@
+//
+// game.swift
+//  Video Game Library
+//
+//  Created by Zachary Harper on 9/10/18.
+//  Copyright © 2018 Zachary Harper. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var title: String
+    let inOut: Bool
+    let dueDate = Date
+    
+    init(title:String, inOut: Bool, dueDate: Date) {
+        self.title = title
+        self.inOut = inOut
+        self.dueDate = dueDate
+    }
+    
+    func checkOut() {
+        if inOut {
+            print("You've checked out \(title) and its due date is \(dueDate)")
+        }else {
+            print("You've not checked out a game")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
